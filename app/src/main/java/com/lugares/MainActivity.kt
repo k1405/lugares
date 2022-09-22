df56d9bc14473bd9f43be7d263b903da5e422f24
k1405 <63481511+k1405@users.noreply.github.com>
2022-09-21 21:14:56 -0600

    version original clase 1

---
+++ app/src/main/java/com/lugares/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.lugares
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.lugares.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    // definicion de objeto para hacer la autenticacion
+    private lateinit var auth : FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+    }
+}
