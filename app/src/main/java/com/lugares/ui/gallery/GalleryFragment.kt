03138144e167eb978def544d18edc6159abb1ab4
k1405 <63481511+k1405@users.noreply.github.com>
2022-12-01 21:06:20 -0600

    clases final

--- app/src/main/java/com/lugares/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/lugares/ui/gallery/GalleryFragment.kt
@@ -7,16 +7,59 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.lugares.databinding.FragmentGalleryBinding
 import com.lugares.viewmodel.GalleryViewModel
 
-class GalleryFragment : Fragment() {
+class GalleryFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentGalleryBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var googleMap: GoogleMap
+    private var mapReady = false;
+
+    private lateinit var lugarViewModel: LugarViewModel
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.map.onCreate(savedInstanceState)
+        binding.map.onResume()
+        binding.map.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        map.let{
+            googleMap= it
+            mapReady=true
+
+
+            lugarViewModel.getLugares.observe(viewLifecycleOwner){lugares -> updatedmap(lugares)}
+
+        }
+    }
+
+    private fun updatedmap(lugares: List<Lugar>) {
+
+        if (mapReady){
+
+            lugares.forEach{ lugar->
+                if(lugar.latitud?.isFinite()==true && lugar.longitud?.isFinite()==true){
+                    val marca = LatLng(lugar.latitud,lugar.longitud)
+                    googleMap.addMarker(MarkerOptions().position(marca).title(lugar.nombre))
+
+                }
+            }
+
+        }
+
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,16 +67,13 @@ class GalleryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+            ViewModelProvider(this)[(LugarViewModel::class.java]
 
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+
+
+        return binding.root
     }
 
     override fun onDestroyView() {
